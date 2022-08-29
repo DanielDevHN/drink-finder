e2da87a5889668ec245ceaf51e2ac6d5b5eed428
DanielDevHN <dangrereyes@gmail.com>
2022-08-28 20:46:27 -0600

    Agregando estructura principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { Container} from 'react-bootstrap'
+
 
 function App() {
   
-
   return (
-    <div>Bebidas</div>
+    <>
+      <header className="py-5">
+        <h1>Buscador de Bebidas</h1>
+      </header>
+
+      <Container className="mt-5">
+        <p>Hola Mundo</p>
+      </Container>
+
+      <footer className="mt-5">
+        <div>Made with ❤ by Daniel Reyes</div>
+      </footer>
+
+    </>
   )
 }
 
