afe7ecaea0993271a678ed6303dd06b55222ab5d
DanielDevHN <dangrereyes@gmail.com>
2022-08-29 15:09:25 -0600

    Se agregó modal para las bebidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap'
 import Formulario from './components/Formulario'
 import ListadoBebidas from './components/ListadoBebidas'
+import ModalBebida from './components/ModalBebida'
 import { CategoriasProvider } from './context/CategoriasProvider'
 import { BebidasProvider } from './context/BebidasProvider'
 
@@ -16,6 +17,8 @@ function App() {
         <Container className="mt-5">
           <Formulario />
           <ListadoBebidas />
+
+          <ModalBebida />
         </Container>
 
         <footer className="py-5">
