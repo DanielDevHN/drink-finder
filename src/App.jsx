fab4251197a1c656af6c3e2777b408237a708e03
DanielDevHN <dangrereyes@gmail.com>
2022-08-28 21:26:28 -0600

    Se agregó el provider al estructura principal de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Container} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import Formulario from './components/Formulario'
+import { CategoriasProvider } from './context/CategoriasProvider'
 
 function App() {
-  
+
   return (
-    <>
+    <CategoriasProvider>
       <header className="py-5">
         <h1>Buscador de Bebidas</h1>
       </header>
@@ -16,8 +17,7 @@ function App() {
       <footer className="mt-5">
         <div>Made with ❤ by Daniel Reyes</div>
       </footer>
-
-    </>
+    </CategoriasProvider>
   )
 }
 
