fe4b437e3558acd36dba96abc9013c62e7da029f
DanielDevHN <dangrereyes@gmail.com>
2022-08-28 21:24:51 -0600

    Se agregó custom hook para categorías

---
+++ src/hooks/useCategorias.jsx
@@ -0,0 +1,11 @@
+import { useContext } from 'react'
+import CategoriasContext from '../context/CategoriasProvider'
+
+
+const useCategorias = () => {
+    return useContext(CategoriasContext)
+}
+
+
+
+export default useCategorias
