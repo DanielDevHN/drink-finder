6d7ce6499a440ce4c5d1085723f5772feb925186
DanielDevHN <dangrereyes@gmail.com>
2022-08-28 21:10:55 -0600

    Agregando componente para formulario de búsqueda

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,36 @@
+import { Button, Form, Row, Col } from 'react-bootstrap'
+
+
+
+const Formulario = () => {
+  return (
+    <Form>
+        <Row>
+            <Col md={6}>
+                <Form.Group className="mb-3">
+                    <Form.Label htmlFor='nombre'>Nombre Bebida</Form.Label>
+                    <Form.Control
+                        id='nombre'
+                        type='text'
+                        placeholder='Ej: Tequila, Vodka, etc'
+                        name='nombre'
+                    />
+                </Form.Group>
+            </Col>
+            <Col md={6}>
+            <Form.Group className="mb-3">
+                    <Form.Label htmlFor='categoria'>Categoria Bebida</Form.Label>
+                    <Form.Select
+                        id='categoria'
+                        name='categoria'
+                    >
+                        <option>- Selecciona Categoria -</option>
+                    </Form.Select>
+                </Form.Group>
+            </Col>
+        </Row>
+    </Form>
+  )
+}
+
+export default Formulario
