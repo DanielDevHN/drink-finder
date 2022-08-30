b84eea25d87872849dd0f1d4d56d9c84761da9c9
DanielDevHN <dangrereyes@gmail.com>
2022-08-29 22:09:10 -0600

    Agregando método para obtener datos de la api en el modal

--- src/components/ModalBebida.jsx
+++ src/components/ModalBebida.jsx
@@ -1,18 +1,43 @@
-import { Modal, Image} from 'react-bootstrap'
+import { Modal, Image } from 'react-bootstrap'
 import useBebidas from '../hooks/useBebidas'
 
 const ModalBebida = () => {
 
-    const { modal, handleModalClick } = useBebidas()
+    const { modal, handleModalClick, receta } = useBebidas()
 
+    const mostrarIngredientes = () => {
+        let ingredientes = []
 
-  return (
-    <Modal show={modal} onHide={handleModalClick}>
-        <Modal.Body>
-            Cuerpo del Modal
-        </Modal.Body>
-    </Modal>
-  )
+        for (let i = 1; i < 16; i++) {
+            if( receta[`strIngredient${i}`]){
+                ingredientes.push(
+                    <li>{receta[`strIngredient${i}`]} {receta[`strMeasure${i}`]}</li>
+                )
+            }
+            
+        }
+        return ingredientes
+    }
+
+    return (
+        <Modal show={modal} onHide={handleModalClick}>
+            <Image
+                src={receta.strDrinkThumb}
+                alt={`Imagen receta ${receta.strDrink}`}
+            />
+            <Modal.Header>
+                <Modal.Title>{receta.strDrink}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className='p-3'>
+                    <h2>Instrucciones</h2>
+                    {receta.strInstructions}
+                    <h2>Ingredientes y Cantidades</h2>
+                    {mostrarIngredientes()}
+                </div>
+            </Modal.Body>
+        </Modal>
+    )
 }
 
 export default ModalBebida
