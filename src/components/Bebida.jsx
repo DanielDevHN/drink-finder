3c05dcf74a596b2ff6367c26b56536ef0c245f4f
DanielDevHN <dangrereyes@gmail.com>
2022-08-29 19:09:05 -0600

    Se agregó metodo para obtener id de bebida desde la api

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -4,7 +4,7 @@ import useBebidas from '../hooks/useBebidas'
 
 const Bebida = ({ bebida }) => {
 
-    const { handleModalClick } = useBebidas()
+    const { handleModalClick, handleBebidaIdClick } = useBebidas()
 
     return (
         <Col md={6} lg={3}>
@@ -22,6 +22,7 @@ const Bebida = ({ bebida }) => {
                         className='w-100 mt-2'
                         onClick={() => {
                             handleModalClick()
+                            handleBebidaIdClick(bebida.idDrink)
                         }}
                     >
                         Ver Receta
