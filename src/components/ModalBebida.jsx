ac7076746693c140eb474e8b43d5ca6a394491f1
DanielDevHN <dangrereyes@gmail.com>
2022-08-29 22:25:08 -0600

    Se agregó validación para mostrar la receta seleccionada

--- src/components/ModalBebida.jsx
+++ src/components/ModalBebida.jsx
@@ -3,7 +3,7 @@ import useBebidas from '../hooks/useBebidas'
 
 const ModalBebida = () => {
 
-    const { modal, handleModalClick, receta } = useBebidas()
+    const { modal, handleModalClick, receta, cargando } = useBebidas()
 
     const mostrarIngredientes = () => {
         let ingredientes = []
@@ -13,30 +13,31 @@ const ModalBebida = () => {
                 ingredientes.push(
                     <li>{receta[`strIngredient${i}`]} {receta[`strMeasure${i}`]}</li>
                 )
-            }
-            
+            } 
         }
         return ingredientes
     }
 
     return (
-        <Modal show={modal} onHide={handleModalClick}>
-            <Image
-                src={receta.strDrinkThumb}
-                alt={`Imagen receta ${receta.strDrink}`}
-            />
-            <Modal.Header>
-                <Modal.Title>{receta.strDrink}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div className='p-3'>
-                    <h2>Instrucciones</h2>
-                    {receta.strInstructions}
-                    <h2>Ingredientes y Cantidades</h2>
-                    {mostrarIngredientes()}
-                </div>
-            </Modal.Body>
-        </Modal>
+        !cargando && (
+            <Modal show={modal} onHide={handleModalClick}>
+                <Image
+                    src={receta.strDrinkThumb}
+                    alt={`Imagen receta ${receta.strDrink}`}
+                />
+                <Modal.Header>
+                    <Modal.Title>{receta.strDrink}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className='p-3'>
+                        <h2>Instrucciones</h2>
+                        {receta.strInstructions}
+                        <h2>Ingredientes y Cantidades</h2>
+                        {mostrarIngredientes()}
+                    </div>
+                </Modal.Body>
+            </Modal>
+        )
     )
 }
 
